d28b16f0d37346df0e2f0ea1cca18e25e46ee4bd
Mercia Jeno <mercia.jeno@gmail.com>
2026-03-05 22:21:08 +0530

    Pixels for github and leetcode created

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
 import requests
-USERNAME = 'mercia'
-TOKEN = 'iamahackerandadetective'
-pixel_endpoint='https://pixe.la/v1/users'
-user_params={
-    'token':TOKEN,
-    'username':USERNAME,
-    'agreeTermsOfService':'yes',
-    'notMinor':'yes'
-}
-#response = requests.post(pixel_endpoint,json=user_params)
-#print(response.text)
-graph_params = {
-    "id": "coding",
-    "name": "Coding Tracker",
-    "unit": "commits",
-    "type": "int",
-    "color": "shibafu"
+from datetime import datetime
+
+USERNAME = "mercia"
+TOKEN = "iamahackerandadetective"
+BASE_URL = "https://pixe.la/v1/users"
+
+headers = {
+    "X-USER-TOKEN": TOKEN
 }
 
-headers={
-    'X-USER-TOKEN':TOKEN
-}
-graph_endpoint = f'{pixel_endpoint}/{USERNAME}/graphs'
-response = requests.post(graph_endpoint,json=graph_params,headers=headers)
-print(response.text)
+today = datetime.now().strftime("%Y%m%d")
+
+# --------- GRAPH DEFINITIONS ----------
+graphs = [
+    {
+        "id": "coding",
+        "name": "Coding Tracker",
+        "unit": "commits",
+        "type": "int",
+        "color": "shibafu"
+    },
+    {
+        "id": "github",
+        "name": "Github Tracker",
+        "unit": "commits",
+        "type": "int",
+        "color": "kuro"
+    }
+]
+
+
+# --------- FUNCTIONS ----------
+
+def create_graph(graph_data):
+    endpoint = f"{BASE_URL}/{USERNAME}/graphs"
+    response = requests.post(endpoint, json=graph_data, headers=headers)
+    print(f"Create graph {graph_data['id']} -> {response.text}")
+
+
+def post_pixel(graph_id, quantity):
+    endpoint = f"{BASE_URL}/{USERNAME}/graphs/{graph_id}"
+    data = {
+        "date": today,
+        "quantity": str(quantity)
+    }
+    response = requests.post(endpoint, json=data, headers=headers)
+    print(f"Post pixel to {graph_id} -> {response.text}")
+
+
+# --------- CREATE GRAPHS ----------
+# for graph in graphs:
+#     create_graph(graph)
+
+
+# --------- ADD PIXELS ----------
+post_pixel("coding", 3)
+post_pixel("github", 5)
