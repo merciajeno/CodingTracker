01e92a3849c548543811c3ee60e3fbfa5ea66d46
Mercia Jeno <mercia.jeno@gmail.com>
2026-03-06 14:33:33 +0530

    github tracker added

---
+++ github_service.py
@@ -0,0 +1,8 @@
+import requests
+
+github_url = 'https://api.github.com/search/commits?q='
+
+def get_commits(username):
+    response = requests.get(github_url+f'{username}'+'committer-date')
+    print(response.json()['total_count'])
+get_commits('merciajeno')
