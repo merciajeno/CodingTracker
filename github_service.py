0919d133407193cf5017a99ddd2e0b525be0a355
Mercia Jeno <mercia.jeno@gmail.com>
2026-03-15 19:31:12 +0530

    found recent commits for today

--- github_service.py
+++ github_service.py
@@ -1,8 +1,27 @@
 import requests
-
-github_url = 'https://api.github.com/search/commits?q='
-
+import datetime
+github_url = "https://api.github.com/search/commits"
+today = datetime.datetime.now()
 def get_commits(username):
-    response = requests.get(github_url+f'{username}')
-    print(response.json()['total_count'])
-get_commits('merciajeno')
+    params = {
+        "q": f"author:{username}",
+        "sort": "author-date",
+        "order": "desc"
+    }
+
+    headers = {
+        "Accept": "application/vnd.github.cloak-preview"
+    }
+
+    response = requests.get(github_url, params=params, headers=headers)
+
+    data = response.json()
+
+    for item in data["items"]:
+        commit = item["commit"]
+        date = commit["author"]["date"]
+        date=date[:date.find('T')]
+        if today.strftime('%Y-%m-%d')==date:
+         print(commit["author"]["date"], "-", commit["message"])
+
+get_commits("merciajeno")
